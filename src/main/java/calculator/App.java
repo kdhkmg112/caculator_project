d25dfd65d840d7848d89a570659174df85e7aa4f
dd <dd@naver.com>
2024-07-28 04:26:33 +0900

    initial commit

---
+++ src/main/java/calculator/App.java
@@ -0,0 +1,54 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String operator = sc.nextLine();
+
+            int result = 0;
+            switch (operator) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        System.out.println("분모가 0이므로 계산이 불가능합니다.");
+                        break;
+                    } else {
+                        result = num1 / num2;
+                        break;
+                    }
+            }
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+            String str = sc.nextLine();
+            if (str.equals("exit")) {
+                break;
+            }
+
+        }
+
+
+
+    }
+}
