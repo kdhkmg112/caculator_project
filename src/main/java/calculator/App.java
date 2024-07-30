bb7dc5b6b0cae7255e49f534969e5195ae970663
dd <dd@naver.com>
2024-07-30 11:27:34 +0900

    2-8 ...

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -52,7 +52,6 @@ public class App {
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String checkExit = sc.next();
             if (checkExit.equals("exit")) {
-
                 break;
             }
         }
