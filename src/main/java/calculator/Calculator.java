f31732e4314d972085224ad9f3c0935ce96aeca9
dd <dd@naver.com>
2024-07-30 10:29:22 +0900

    2-1 클래스 생성 / 연산 결과 반환 메서드 / 컬렉션 타입필드 생성 (throw구현 실패 ㅠ)

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,31 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
 
+public class Calculator {
+    static private ArrayList<Double> doubleList = new ArrayList<>();
+
+    public static double calculate(int num1, int num2, char operator) {
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("분모가 0이므로 계산이 불가능합니다.");
+                    break;
+                } else {
+                    result = (double) num1 / num2;
+                    break;
+                }
+        }
+        return result;
+    }
 }
